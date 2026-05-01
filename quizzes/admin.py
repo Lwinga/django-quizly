5f9dc4c21e7b861f9a3f0a5d7d2916375f0c10c5
Kelvin Ambokile Lwinga <kelvinjohnvin449@gmail.com>
2026-05-01 21:05:01 +0300

    Customize admin site for better UX

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -2,14 +2,32 @@ from django.contrib import admin
 from .models import Quiz, Question, Choice
 
 
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 3
+    fields = ['text']
+    show_change_link = True
+
+
+class QuizAdmin(admin.ModelAdmin):
+    fields = ['title', 'description']
+    inlines = [QuestionInline]
+    list_display = ['title', 'description', 'is_active', 'created_at']
+    search_fields = ['title', 'description']
+
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 2
+    fields = ['text', 'is_correct']
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    fields = ['quiz', 'text']
     inlines = [ChoiceInline]
+    list_display = ['text', 'quiz', 'created_at']
+    search_fields = ['text']
 
 
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question, QuestionAdmin)
