03cb744980adf91fe44853c36ac2bff028a4da64
Kelvin Ambokile Lwinga <kelvinjohnvin449@gmail.com>
2026-05-01 19:11:59 +0300

    Display choices of each question

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Quiz, Question
+from .models import Quiz, Question, Choice
 
-admin.site.register([Quiz, Question])
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Quiz)
+admin.site.register(Question, QuestionAdmin)
